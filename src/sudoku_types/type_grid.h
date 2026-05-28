d70f414f3476cffe66238a4be4ced27461c3bd68
Arno-Dubois <arno.dubois@student.junia.com>
2026-05-28 09:25:51 +0200

    Added types of sudoku

---
+++ src/sudoku_types/type_grid.h
@@ -0,0 +1,8 @@
+#ifndef TYPE_GRID_H
+#define TYPE_GRID_H
+
+#include "type_sudokutile.h"
+
+typedef SudokuTile grid[81];
+
+#endif /* TYPE_GRID_H */
---
+++ src/sudoku_types/type_subset.h
@@ -0,0 +1,8 @@
+#ifndef TYPE_SUBSET_H
+#define TYPE_SUBSET_H
+
+#include "type_sudokutile.h"
+
+typedef SudokuTile **subset;
+
+#endif /* TYPE_SUBSET_H */
