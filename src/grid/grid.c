960c5c9e08ae1d2a109ffeea2d0a7282f9ce261e
Arno-Dubois <arno.dubois@student.junia.com>
2026-06-01 14:07:55 +0200

    merge conflict

--- src/grid/grid.c
+++ src/grid/grid.c
@@ -1,3 +1,57 @@
-#include "grid.h"
+#include "grid_internal.h"
 
-void grid_placeholder(void) {}
+Grid createGrid() {
+  Grid grid = malloc(sizeof(SudokuTile) * NUMBER_OF_TILE_IN_A_GRID);
+  if (grid == NULL)
+    return NULL;
+
+  char possible[NUMBER_OF_POSSIBLE] = {0};
+  for (unsigned char i = 0; i < NUMBER_OF_TILE_IN_A_GRID; i++) {
+    SudokuTile *newTile = createTile(0, possible);
+    grid[i] = *newTile;
+  }
+
+  return grid;
+}
+
+void deleteGrid(Grid grid) {
+  if (grid == NULL)
+    return;
+  for (unsigned char i = 0; i < NUMBER_OF_TILE_IN_A_GRID; i++) {
+    deleteTile(&grid[i]);
+  }
+  free(grid);
+}
+
+char gridGetValueXY(Grid grid, unsigned char x, unsigned char y) {
+  if (grid == NULL || x > NUMBER_OF_TILE_IN_A_GRID / 2 ||
+      y > NUMBER_OF_TILE_IN_A_GRID / 2)
+    return 0;
+
+  return tileGetValue(&grid[(y - 1) * 9 + (x - 1)]);
+}
+
+char *gridGetPossibleXY(Grid grid, unsigned char x, unsigned char y) {
+  if (grid == NULL || x > NUMBER_OF_TILE_IN_A_GRID / 2 ||
+      y > NUMBER_OF_TILE_IN_A_GRID / 2)
+    return NULL;
+
+  return tileGetPossible(&grid[(y - 1) * 9 + (x - 1)]);
+}
+
+char gridSetValueXY(Grid grid, unsigned char x, unsigned char y, char value) {
+  if (grid == NULL || x > NUMBER_OF_TILE_IN_A_GRID / 2 ||
+      y > NUMBER_OF_TILE_IN_A_GRID / 2) {
+    return 1;
+  }
+
+  return tileSetValue(&grid[(y - 1) * 9 + (x - 1)], value);
+}
+char gridSetPossibleXY(Grid grid, unsigned char x, unsigned char y,
+                       char possible[NUMBER_OF_POSSIBLE]) {
+  if (grid == NULL || x > NUMBER_OF_TILE_IN_A_GRID / 2 ||
+      y > NUMBER_OF_TILE_IN_A_GRID / 2)
+    return 1;
+
+  return tileSetPossible(&grid[(y - 1) * 9 + (x - 1)], possible);
+}
