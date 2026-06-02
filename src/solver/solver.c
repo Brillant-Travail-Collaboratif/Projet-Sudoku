9964d8d6fc80e4ba18f4dc77a3d9bb4fabb3c24a
Meikray <f5ch5jppfh@privaterelay.appleid.com>
2026-06-02 11:36:14 +0200

    I create cleanline

--- src/solver/solver.c
+++ src/solver/solver.c
@@ -12,5 +12,21 @@ char removeCandidate(SudokuTile *tile, char value) {
 }
 
 char cleanLine(SudokuTile *line) {
+    if (line == NULL) return 0;
+
+    char modified = 0;
+    for (unsigned char i = 0; i < TILES_PER_LINE; i++) {
+        const char value = line[i].value;
+        if (value == 0) continue; /* case inconnue : rien a propager */
+
+        /* On retire cette valeur des candidats des autres cases inconnues. */
+        for (unsigned char j = 0; j < TILES_PER_LINE; j++) {
+            if (j == i) continue;
+            if (line[j].value != 0) continue;
+            if (removeCandidate(&line[j], value)) modified = 1;
+        }
+    }
+    return modified;
+}
 
 }
