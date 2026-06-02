691e5718d73ae79f906a03ec403b0053b29e3117
Arno-Dubois <arno.dubois@student.junia.com>
2026-06-02 11:17:55 +0200

    made hidden_singles_in_line

--- src/solver/solver.c
+++ src/solver/solver.c
@@ -1,3 +1,20 @@
 #include "solver.h"
 
 void solver_placeholder(void) {}
+
+char solve_hidden_singles_in_line(SudokuTile *line) {
+  for (unsigned char i = 1; i <= NUMBER_OF_POSSIBLE; i++) {
+    unsigned char localisationOfCandidate = 0;
+    for (unsigned char j = 0; j < NUMBER_OF_POSSIBLE; j++) {
+      char candidateValue = tileGetPossible(&line[j])[i];
+      if (candidateValue != 0 && localisationOfCandidate != 0)
+        localisationOfCandidate = j;
+      else if (candidateValue != 0) {
+        localisationOfCandidate = 0;
+        break;
+      }
+    }
+    if (localisationOfCandidate != 0)
+      tileSetValue(&line[localisationOfCandidate], i);
+  }
+}
--- src/solver/solver.h
+++ src/solver/solver.h
@@ -1,4 +1,10 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
+#include "../grid/grid.h"
+#include "../grid/tile.h"
+#include "../sudoku_types/type_sudokutile.h"
+
+char solve_hidden_singles_in_line(SudokuTile *line);
+
 #endif /* SOLVER_H */
