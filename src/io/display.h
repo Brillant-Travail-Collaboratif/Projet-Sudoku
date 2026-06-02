759395df8ad11f22a7c877bfa1027c71592bb7b9
Sensitor <nicolas.gouaux@gmail.com>
2026-06-02 14:44:02 +0200

    disp_subset function done

--- src/io/display.h
+++ src/io/display.h
@@ -2,8 +2,10 @@
 #define DISPLAY_H
 
 #include "../grid/grid.h"
+#include "../sudoku_types/type_subset.h"
 
 void displayFinal(Grid grid);
 void displayPossible(Grid grid);
+void dispSubset(Subset s);
 
 #endif /* DISPLAY_H */
