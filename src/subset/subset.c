ccf244c1de796d6f16cf4d0774699a5ca7087f57
Arno-Dubois <arno.dubois@student.junia.com>
2026-06-02 13:52:13 +0200

    Created getSubdqSubset

--- src/subset/subset.c
+++ src/subset/subset.c
@@ -3,22 +3,47 @@
 //
 
 #include "subset_internal.h"
+#include <stdlib.h>
 
 Subset allocateSubset(void) {
-    return malloc(sizeof(SudokuTile *) * SUBSET_SIZE);
+  return malloc(sizeof(SudokuTile *) * SUBSET_SIZE);
 }
 Subset getLineSubset(Grid grid, int n) {
-    if (grid == NULL ||  n < 0 || n >= SUBSET_SIZE) return NULL;
+  if (grid == NULL || n < 0 || n >= SUBSET_SIZE)
+    return NULL;
 
-    Subset subset = allocateSubset();
-    if (subset == NULL) return NULL;
+  Subset subset = allocateSubset();
+  if (subset == NULL)
+    return NULL;
 
-    for (int k = 0; k < SUBSET_SIZE; k++)
-        subset[k] = &grid[n * SUBSET_SIZE + k];
+  for (int k = 0; k < SUBSET_SIZE; k++)
+    subset[k] = &grid[n * SUBSET_SIZE + k];
 
-    return subset;
+  return subset;
 }
 
 void deleteSubset(Subset subset) {
-    if (subset != NULL) free(subset);
+  if (subset != NULL)
+    free(subset);
+}
+
+Subset getSubsqSubset(Grid grid, int n) {
+  if (grid == NULL || n < 0 || n >= SUBSET_SIZE)
+    return NULL;
+
+  Subset subset = allocateSubset();
+  if (subset == NULL)
+    return NULL;
+
+  unsigned char boxCol = (n % BOX_SIDE) * BOX_SIDE;
+
+  for (int i = 0; i < BOX_SIDE; i++) {
+    for (int j = 0; j < BOX_SIDE; j++) {
+      unsigned char row = n + i;
+      unsigned char col = boxCol + j;
+      subset[i * BOX_SIDE + j] = &grid[row * SUBSET_SIZE + col];
+    }
+  }
+
+  return subset;
 }
