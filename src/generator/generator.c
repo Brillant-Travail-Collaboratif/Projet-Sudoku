32648fd23f9ee5ad72ba6416749059050ffc6115
Sensitor <nicolas.gouaux@gmail.com>
2026-06-10 09:48:16 +0200

    generator files

---
+++ src/generator/generator.c
@@ -0,0 +1,5 @@
+//
+// Created by Nicolas Gouaux on 10/06/2026.
+//
+
+#include "generator.h"
---
+++ src/generator/generator.h
@@ -0,0 +1,18 @@
+//
+// Created by Nicolas Gouaux on 10/06/2026.
+//
+
+#ifndef PROJET_SUDOKU_GENERATOR_H
+#define PROJET_SUDOKU_GENERATOR_H
+
+#include "../sudoku_types/type_grid.h"
+
+
+
+
+
+char generate_grid();
+
+
+
+#endif //PROJET_SUDOKU_GENERATOR_H
